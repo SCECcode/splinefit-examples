c21d8f48dba05ec8b5f258f0578bc442c2ec8826
Ossian O'Reilly <ooreilly@usc.edu>
2019-09-28 21:08:00 -0700

    Rename Sierra Madre.

---
+++ sierra_madre/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+url=https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM5/CFM52_preferred/500m
+files=(WTRA-SMFZ-SMDE-Sierra_Madre_Cucamonga_connector-CFM5_m500.ts
+WTRA-SMFZ-SMDE-Sierra_Madre_fault_low_dip-CFM4_m500.ts)
+mkdir -p meshes
+for file in ${files[@]}
+do
+        curl $url/${file} --output meshes/${file}
+done
