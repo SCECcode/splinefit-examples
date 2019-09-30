cfb4d26bf1ef27419729a019902913a4acd7a29e
Ossian O'Reilly <ooreilly@usc.edu>
2019-09-29 20:54:25 -0700

    Add tutorial 3.

--- sierra_madre/download.sh
+++ sierra_madre/download.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 url=https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/cfm/CFM5/CFM52_preferred/500m
-files=(WTRA-SMFZ-SMDE-Sierra_Madre_Cucamonga_connector-CFM5_m500.ts
-WTRA-SMFZ-SMDE-Sierra_Madre_fault_low_dip-CFM4_m500.ts)
+files=(
+WTRA-SMFZ-MULT-Sierra_Madre_fault_west-CFM4_m500.ts
+WTRA-SMFZ-SWCC-Sierra_Madre_fault_low_dip-CFM4_m500.ts
+WTRA-SMFZ-SMDE-Sierra_Madre_fault_low_dip-CFM4_m500.ts
+WTRA-SMFZ-SMDE-Sierra_Madre_Cucamonga_connector-CFM5_m500.ts
+WTRA-SMFZ-SMDD-Sierra_Madre_fault_low_dip-CFM4_m500.ts
+)
+
 mkdir -p meshes
 for file in ${files[@]}
 do
